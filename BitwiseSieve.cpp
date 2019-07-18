9f8da274bc9c533b5b21d1054de4d942c42294b2
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2019-07-18 23:26:37 +0600

    Update BitwiseSieve.cpp

--- BitwiseSieve.cpp
+++ BitwiseSieve.cpp
@@ -1,6 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector< ll > v ;
+const int MAXN = 1e7 ;
+bitset< MAXN > status ;
+
+void findPrime()
+{
+    int rt = sqrt( MAXN ) + 1 ;
+
+    for(int i = 3; i < rt; i+=2)
+    {
+        if( !status[ i ] )
+        {
+            for( int j = i * i ; j < MAXN ; j += ( i << 1 ) )
+            {
+                 status[j] = true;
+            }
+        }
+    }
+
+    v.push_back(2);
+
+    for(int i = 3; i < MAXN; i+=2)
+    {
+        if(!status[i])
+        {
+            v.push_back(i);
+        }
+    }
+}
+
+int main()
+{
+    sieve();
+    for(auto x : v)
+        cout<<x<<" ";
+}
+
+/*
 vector<int>v;
 const int MAXN = 1e3;
 bitset<MAXN>status;
@@ -21,11 +59,5 @@ void sieve()
         {
             v.push_back(i);
         }
-}
+}*/
 
-int main()
-{
-    sieve();
-    for(auto x : v)
-        cout<<x<<" ";
-}
